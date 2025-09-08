274af2e0e29c8f17a3930b5d16fccac5f8bbc598
Hiengia123 <hien22cdpkthn@gmail.com>
2025-09-08 08:43:53 +0700

    add file app.cs

---
+++ app.cs
@@ -0,0 +1,25 @@
+/*
+-namespace
+decimal nums = 3.4m
+-Parse && TryParse
+-as && is
+-nums[^1] && nums[^2]...
+-range (array)
+-int[,] a = new int[2,3] : 2D
+-int[,,] : 3D
+-constructor : this()
+-getter and setter(property)
+--------
+namespace
+using
+type of data
+string
+object type
+var
+array
+list
+for , if else
+input
+parseInt,..
+OOP
+*/
